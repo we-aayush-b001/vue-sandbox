3aa6657c41c297798f196c5d209e26ed9cd18d79
aayush-b001 <aayush-b001@webreinvent.com>
2026-02-18 15:43:34 +0530

    Passing props from Parent to child component and how to validate them

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,22 @@
   <Counter />
   <br/>
   <calc />
+  <!-- <SinglePost  :postTitle = "postTitle" :id="postId"/> -->
+   <SinglePost v-bind="postDetails"/>
 </template>
 
 
 <script setup>
+import { ref } from 'vue';
 import calc from './components/Calc.vue';   // Local Registration(Tree Shaking friendly)
+import SinglePost from './components/SinglePost.vue';
+
+const postTitle = ref("Communicatipn from Parent to Child Component");
+const postId = ref(3);
+const postDetails = {
+   postTitle : "Communicatipn from Parent to Child Component",
+   id: 3
+}
 </script>
 
 
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,34 @@
+<template>
+ <h1>{{ postTitle }}-{{ id }}</h1>
+</template>
+
+<script setup>
+const props = defineProps({
+    // postTitle : String,
+    // postTitle : [Number ,String],
+    postTitle : {
+        type: String,
+        required : true,  //default value is false
+    },
+    id: Number,
+    postDetails : {
+        type : Object,
+        default(rawProps){
+            return {title:'Default', id : 100};
+        }
+    },
+    message : {
+      validator(value){
+        return ['success', 'warning']
+      }
+    },
+    updateTitle : {
+        type : Function,
+        default(){
+            return "Example for passing function as a prop"
+        }
+    }
+})
+
+console.log(props.postTitle+"===>");
+</script>
